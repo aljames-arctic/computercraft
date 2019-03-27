09a588992c5208308a1ef8bd6d91b769d4f7475a
Alexander James <aljames.arctic@gmail.com>
2019-03-26 20:46:47 -0500

    Quarry v2
    Initial commit of updated Quarry script

--- standard/quarry.lua
+++ standard/quarry.lua
@@ -1,310 +1,234 @@
+local self = {}
 local state = dofile("/apis/state")
-local slot16iscobble = dofile("/apis/state")
 local turtle = dofile("/apis/turtle")
 local pmemory = dofile("/apis/pmemory")
 
-local checkpoint
-if pmemory.add("checkpoint") then
-  checkpoint = turtle.position()
-  checkpoint.f = turtle.facing()
-  pmemory.write("checkpoint", checkpoint, "table")
+self.debug = false
+self.name = "Quarry.exe"
+self.running = true
+
+-- State Machine
+local UNINITIALIZED = 0   -- done
+local BURROW = 1          -- done
+local DIG_FORWARD = 2     -- done
+local TURN = 3            -- done
+local LAYER_END = 6       -- done
+local END_PROGRAM = 7     -- done
+local RETURN_FUEL = 8
+local RETURN_INVENTORY = 9
+local RETURN_JOB = 10
+
+local HOME_FACE = 2
+local home = turtle.position()
+local init_state_variables = { home=home }
+local state_variables = nil
+
+local function gohome()
+  turtle.go({"z:"..home.z, "x:"..home.x, "y:"..home.y, "f:"..HOME_FACE} )
 end
 
-if pmemory.add("direction") then pmemory.write("direction","right","string") end
-if pmemory.add("row") then pmemory.write("row",0,"number") end
+local function return_fuel()
+  gohome()
 
-args = {...}
-if pmemory.add("diameter") or pmemory.read("diameter","number") == nil then
-  if #args ~= 1 then
-    print "startup <diameter>"
-    print "RECOMMENDED : bucket in inventory slot 1"
-    return
-  elseif tonumber(args[1]) < 1 then
-    print "Requires a positive diameter"
-    return
+  term.setCursorPos(5,1)
+  print("Waiting on a resupply of fuel")
+  while( turtle.getFuelLevel() < 5000 )
+    turtle.refuel(64)
+    sleep(10)
   end
-  pmemory.write("diameter", tonumber(args[1]), "number")
-end
-local diameter = pmemory.read("diameter","number")
 
--- arg=1 returns true if fuel level <= distance to home
---       returns false otherwise
--- arg=2 returns distance to checkpoint * 2 - fuel level if > 0
---       returns false otherwise
-local function require_refuel(n)
-  if n == 1 then
-    local pos = turtle.position()
-    local dist = math.abs(pos.x) + math.abs(pos.y) + math.abs(pos.z) + 10
-    if turtle.getFuelLevel() <= dist then
-      turtle.selectFuel()
-      return not turtle.refuel(1)
+  state.set(RETURN_JOB)
+end
+
+local function return_inventory()
+  gohome()
+
+  for i = 1, 16, 1 do
+    local attempt = 0
+    while turtle.getItemCount > 0 and turtle.drop() == false then
+      term.setCursorPos(5,1)
+      attempt += 1
+      print("Inventory full... attempt #"..attempt)
+      sleep(10)
     end
-    return false
-
-  elseif n == 2 then
-    local pos1 = pmemory.read("checkpoint","table")
-    local pos2 = turtle.position()
-    
-    -- calculate distance from current location to the checkpoint
-    -- and from the checkpoint back to 0,0,0
-    local dist = math.abs(pos1.x-pos2.x) + math.abs(pos1.y-pos2.y) + math.abs(pos1.z-pos2.z)
-          dist = dist + math.abs(pos1.x) + math.abs(pos1.y) + math.abs(pos1.z)
-
-    if turtle.getFuelLevel() < dist then
-      repeat turtle.refuel(1)
-      until not turtle.selectFuel() or turtle.getFuelLevel() >= dist
-      if turtle.getFuelLevel() < dist then
-        return dist - turtle.getFuelLevel()
-      end
-    end
-    return 0
-
   end
-  return true
+
+  state.set(RETURN_JOB)
 end
 
--- cobblestone goes to slot 16 so just try that
-local function cobblestone_full()
-  if slot16iscobble.get() == 1 and turtle.getItemCount(16) > 32 then
-    turtle.select(16)
-    turtle.drop()
-    slot16iscobble.set(0)
-    turtle.select(2)
-  end
+local function return_job()
+  turtle.to({"x:"..state_variables.saved_pos.x, "y:"..state_variables.saved_pos.y, "z:"..state_variables.saved_pos.z, "f:"..state_variables.saved_f}, true)
+  state.set( state_variables.last_action )
 end
 
--- if 16 slots are full then it drops droppables
--- returns true if inventory is full of non-dropables
--- returns false otherwise
+local function fuel_low()
+  local pos = turtle.position()
+  distance = math.abs(home.x-pos.x) + math.abs(home.y-pos.y) + math.abs(home.z-pos.z)
+  return ( turtle.getFuelLevel() <= distance )
+end
+
 local function inventory_full()
-  cobblestone_full() -- check if cobblestone is full
-  if turtle.emptySlot() then return false end
-  if slot16iscobble.get() == 1 then
-    turtle.select(16)
-    turtle.drop()
-    slot16iscobble.set(0)
-    turtle.select(2)
-  end
-  return true
+  return ( turtle.getItemCount(16) > 0 )
 end
 
--- the turtle is facing chests
--- empty inventory into the chests
--- if the inventory is NOT a chest, return false
--- if the turtle can not unload completely, return false
--- otherwise return true
-local function unload_inventory()
-  _, item = turtle.inspect()
-  if item.name ~= "minecraft:chest" then
-    return false
-  end
+local function end_program()
+  gohome()
 
-  local retval = true
-  local firstfuel = true
-  for i=2,16 do -- our first inventory slot is reserved for a bucket
-    turtle.select(i)
-    if firstfuel and turtle.refuel(0) then
-      firstfuel = false
-    else
-      turtle.drop()
-      retval = retval and turtle.getItemCount(i) == 0
-    end
-  end
-  return retval
+  self.running = false
 end
 
--- mining function
--- check if it is stone/cobblestone 
--- if it is select slot 16
-local function mine(dir)
-  local _, item = turtle.inspect(dir)
-  if item.name == "minecraft:stone" or item.name == "minecraft:cobblestone" then
-    turtle.select(16)
-    slot16iscobble.set(1)
+local function force_return()
+  local position = turtle.position()
 
-  elseif item.name == "minecraft:flowing_lava" then
-    -- grab your bucket and get some free fuel!
-    turtle.select(1)
-    turtle.place(dir)
-    turtle.refuel(1)
-
-  elseif item.name == "minecraft:bedrock" and dir ~= "down" then
-    state.set(5)
-    if dir == "up" then
-      turtle.move("back",true)
-    end
+  if fuel_low() then
+    state_variables.last_action = state.get()
+    state_variables.saved_pos = turtle.position()
+    state_variables.saved_f = turtle.facing()
+    pmemory.write("state_variables", state_variables, "table")
+    state.set(RETURN_FUEL)
+    return true
+  elseif inventory_full() then
+    state_variables.last_action = state.get()
+    state_variables.saved_pos = turtle.position()
+    state_variables.saved_f = turtle.facing()
+    pmemory.write("state_variables", state_variables, "table")
+    state.set(RETURN_INVENTORY)
+    return true
   end
-    
-  return turtle.dig(dir)
+
+  return false
 end
 
-local function task()
-  while true do
-    sleep(0.5)
-    if state.curr == 1 then -- MINING STATE
-      if require_refuel(1) then
-        -- set return position
-        checkpoint = turtle.position()
-        checkpoint.f = turtle.facing()
-        pmemory.write("checkpoint", checkpoint, "table")
-        -- set state to returning for fuel
-        state.set(2)
-      elseif inventory_full() then
-        -- set return position
-        checkpoint = turtle.position()
-        checkpoint.f = turtle.facing()
-        pmemory.write("checkpoint", checkpoint, "table")
-        -- set state to return to deposit
-        state.set(3)
-      else
-        -- continue mining
-        mine("forward")
-        if state.curr ~= 5 then turtle.move("forward",true) end
-        mine("up")
-        mine("down")
+local function layer_end()
+  turtle.turn("left")
+  turtle.turn("left")
+  state.set(BURROW)
+end
 
-        local pos = turtle.position()
-        local dir = pmemory.read("direction","string")
-        local row = pmemory.read("row","number")
+local function turn()
+  if state_variables.turn_count == 1 or state_variables.turn_count == 3 then
+    turtle.move("forward")
+    -- NOTE!!! UNsaved_pos STATE
+  end
 
-        if pos.y == 0 or math.abs(pos.y) == diameter-1 then
-          if (pos.x == 0 or math.abs(pos.x) == diameter-1)
-            and row ~= 0 then
-            pmemory.write("row",0,"number")
-            for i=1,3 do
-              local _, item = turtle.inspect("down")
-              if item.name == "minecraft:bedrock" then
-                if i == 1 or i == 2 then  -- i = 1 : the bottom layer of you you just mined has bedrock 
-                  state.set(5)            -- i = 2 : the top layer of what you will mine has bedrock
-                  break                   -- i = 3 : the middle layer of what you will mine has bedrock -- move up one
-                end 
-                turtle.move("up",true) 
-              end
-              turtle.move("down",true)
-              mine("down")
-            end
-            turtle.turn("back")
-          
-          else
-            pmemory.write("row",row+1,"number")
-            turtle.turn(dir)
-            mine("forward")
-            if state.curr ~= 5 then turtle.move("forward",true) end
-            mine("up")
-            mine("down")
-            turtle.turn(dir)
+  if state_variables.turn_count < 2 then
+    turtle.turn("right")
+  else
+    turtle.turn("left")
+  end
 
-            if dir == "right" then pmemory.write("direction","left","string")
-            else pmemory.write("direction","right","string")
-            end
-          end
-        end
-      end
-    end
+  state_variables.turn_count = (state_variables.turn_count + 1) % 4
+  pmemory.write( "state_variables", state_variables, "table" )
 
-    if state.curr == 2 then -- REFUEL STATE
-      turtle.to({"z:" .. 0, "x:" .. 0, "y:" .. 0, "f:" .. 0 },true)
-      term.clear()
-      term.setCursorPos(1,1)
-      print ("Required fuel ... " .. require_refuel(2))
-      -- wait for fuel then refill
-      repeat
-        os.pullEvent("turtle_inventory")
-      until require_refuel(2) == 0
-      -- set state to return to mining
-      state.set(4)
-    end
+  if state_variables.turn_count == 2 or state_variables.turn_count == 0 then
+    state.set(DIG_FORWARD)
+  end
+end
 
-    if state.curr == 3 then -- UNLOAD STATE
-      turtle.to({"z:" .. 0, "x:" .. 0, "y:" .. 0, "f:" .. 2 },true)
-      print "Unloading ... "
-      local unloaded = unload_inventory()
-      while not unloaded do
-        local _, item = turtle.inspect()
-        if item.name == "minecraft:chest" then
-          if require_refuel(1) or require_refuel(2) > 0 then
-            -- refuel state
-            state.set(2)
-            break
-          else 
-            turtle.move("down",true)
-            unloaded = unload_inventory()
-          end
-        else 
-          -- invalid inventory enter error state
-          state.set(6)
-          break
-        end
-      end
+local function dig_forward()
+  local position = turtle.position()
+  local facing = turtle.facing()
 
-      if unloaded and require_refuel(2) == 0 then
-        state.set(4)
-      else
-        state.set(2)
-      end
-    end
+  row_dist = math.abs(position.x - home.x)
+  start_dist = position.y - home.y
+  end_dist = nil
+  increment = nil
 
-    if state.curr == 4 then -- RETURN TO MINE STATE
-      -- returning from refuel/deposit
-      checkpoint = pmemory.read("checkpoint", "table")
-      turtle.to({"y:" .. checkpoint.y, "x:" .. checkpoint.x, "z:" .. checkpoint.z, "f:" .. checkpoint.f})
-      -- return to mining state
-      state.set(1)
-    end
+  if facing == 0 then
+    end_dist = state_variables.diameter - 1
+    increment = 1
+  else
+    end_dist = 0
+    increment = -1
+  end
 
-    if state.curr == 5 then -- FINISHED STATE
-      -- returning because finished
-      turtle.to({"z:" .. 0, "x:" .. 0, "y:" .. 0, "f:" .. 0 })
-      term.clear()
-      term.setCursorPos(1,1)
-      print "Excavation Completed"
+  for dig=start_dist, end_dist, increment do
+    if force_return() then
       return
     end
 
-    if state.curr == 6 then -- ERROR STATE
-      turtle.to({"z: " .. 0})
-      sleep(1)
-      term.clear()
-      term.setCursorPos(1,1)
-      print "An error occurred!"
-      print "Change my inventory to denote resolution"
-
-      os.pullEvent("turtle_inventory")
-      state.set(3) -- unload current
+    turtle.dig("up")
+    turtle.dig("down")
+    if dig ~= end_dist then
+      turtle.dig("forward")
+      turtle.move("forward")
     end
   end
-end
 
-local function menu()
-  while true do
-    term.clear()
-    term.setCursorPos(1,1)
-    print "Welcome to the Turtle Excavater"
-    print "-------------------------------"
-    print " Press Q to quit"
-
-    local e, k = os.pullEvent("key")
-
-    os.pullEvent("char")
-    term.clear()
-    term.setCursorPos(1,1)
-
-    if k == keys.q then return end
+  if row_dist == diameter then
+    state.set(LAYER_END)
+  else
+    state.set(TURN)
   end
 end
 
-state.initialize()
-slot16iscobble.name = "cobble_state"
-slot16iscobble.initialize()
-if state.curr == 0 then
-  turtle.initialize()
-  state.set(1)
-  mine("up")
-  mine("down")
+local function burrow()
+  local position = turtle.position()
+  local z_dist = math.abs(position.z - home.z)
+  local burrow_again = ( z_dist % 3 ~= 0 ) 
+
+  if force_return() then
+    return
+  end
+
+  -- burrowing further ends the program (might leave a layer or 2 undone)
+  if z_dist + 1 > state_variables.depth then
+    state.set(END_PROGRAM)
+    return
+  end
+
+  if burrow_again then
+    turtle.dig("down")
+    turtle.move("down")
+  else
+    state.set(DIG_FORWARD)
+  end
 end
-parallel.waitForAny( menu, task )
-state.finalize()
-slot16iscobble.finalize()
-pmemory.delete("diameter")
-pmemory.delete("direction")
-pmemory.delete("row")
+
+local function prompt_param()
+  write("Insert a diameter: ")
+  local diameter = tonumber( read() )
+  write("Insert a depth: ")
+  local depth = tonumber( read() )
+
+  state_variables.diameter = diameter
+  state_variables.depth = depth
+  state_variables.home = turtle.position()
+  pmemory.write("state_variables", state_variables, "table")
+  state.set(BURROW)
+end
+
+-- State Machine Functions
+local state_action = 
+{
+  [PROMPT_PARAM] = function prompt_param() end,
+  [BURROW] = function() burrow() end,
+  [DIG_FORWARD] = function() dig_forward() end,
+  [TURN] = function() turn() end,
+  [LAYER_END] = function() layer_end() end,
+  [END_PROGRAM] = function() end_program() end,
+  [RETURN_FUEL] = function() return_fuel() end,
+  [RETURN_INVENTORY] = function() return_inventory() end,
+  [RETURN_JOB] = function() return_job() end,
+}
+
+-- actual task to be run
+function self.run()
+  state.initialize()
+  if state.curr == UNINITIALIZED then
+    turtle.initialize()
+    pmemory.remove("state_variables") -- delete then re-add
+    pmemory.add("state_variables")    -- to reset state
+    pmemory.write("state_variables", init_state_variables, "table") -- initialize
+    state.set(PROMPT_PARAM)
+  end
+
+  -- initialize internal state variables
+  state_variables = pmemory.read("state_variables", "table")
+  home = state_variables.home
+  while self.running do state_action[state.curr]() end
+
+  state.finalize()
+end
+
+return self
