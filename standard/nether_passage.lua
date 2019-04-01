ab919534e34152902ae5014d9709e13e9227ba47
Alexander James <aljames.arctic@gmail.com>
2019-03-31 21:48:39 -0500

    update for multiblock grab and return to home

--- standard/nether_passage.lua
+++ standard/nether_passage.lua
@@ -1,14 +1,27 @@
 -- floor and ceiling
+pmemory = dofile("/apis/pmemory")
 turtle = dofile("/apis/turtle")
 
 length = 0
+slot = 1
+turtle.initialize()
 
 for i = 1, length, 1 do 
     turtle.move("forward", true)
     turtle.dig("down")
-    turtle.place("down")
+    succ, cnt = turtle.place("down")
+    if cnt == 0 then
+        slot = slot + 1
+        turtle.select(slot)
+    end
     turtle.move("up", true)
     turtle.dig("up")
-    turtle.place("up")
+    succ, cnt = turtle.place("up")
+    if cnt == 0 then
+        slot = slot + 1
+        turtle.select(slot)
+    end
     turtle.move("down", true)
 end
+
+turtle.to({"x:0","y:0","z:0"})
