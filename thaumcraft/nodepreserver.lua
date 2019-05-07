dc5fb7c738ae07ed771468d8856482a12ffb9b58
Alexander James <aljames.arctic@gmail.com>
2019-05-07 18:48:18 -0500

    added thaumcraft

---
+++ thaumcraft/nodepreserver.lua
@@ -0,0 +1,16 @@
+-- Node preserver requires Thaumcraft 4
+-- Place the ComputerCraft block directly adjacent to the node
+-- Run a redstone signal from the ComputerCraft block to the Node Stabilizer
+
+node = peripheral.wrap("bottom")
+if node == nil then
+    print "ERROR : No node found"
+end
+
+-- node has two functions, getAspectCount()
+--                         getAspects()
+
+esstable = { node.getAspects() }
+for k,v in pairs(esstable) do
+    print( tostring(k) .. ":" .. tostring(v) )
+end
